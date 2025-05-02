da04891bbf0a90ab09fb5ea2abe745c93218d9d8
formixcode <contact@formixcode.com>
2025-05-02 13:00:02 +0700

    s1

--- site/components/common/Navbar/Navbar.tsx
+++ site/components/common/Navbar/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: FC<NavbarProps> = ({ links }) => (
       <div className={s.nav}>
         <div className="flex items-center flex-1">
           <Link href="/" className={s.logo} aria-label="Logo">
-            <Logo />
+            {/* <Logo /> */}
           </Link>
           <nav className={s.navMenu}>
             <Link href="/search" className={s.link}>
